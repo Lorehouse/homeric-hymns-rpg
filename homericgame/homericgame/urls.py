552e4a20013f190e272c3bd5a0c4545a332034e7
Sarah Monnier <sarahjmonnier@gmail.com>
2024-02-19 20:59:10 -0600

    created home page

--- homericgame/homericgame/urls.py
+++ homericgame/homericgame/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 urlpatterns = [
     path('game/', include('game.urls')),
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name="home.html") )
 ]
 
